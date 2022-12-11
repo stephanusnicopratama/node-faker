f3bdefa9eb6c1c4e70c66ad9250de47b831dbe6d
nico.pratama <nico.pratama@gojek.com>
2022-12-11 17:38:32 +0700

    add message to root

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
-import express from 'express';
-import core from './core';
+import express from "express";
+import core from "./core";
 const router = express();
 const PORT = process.env.PORT || 3000;
 
 router.use(express.json());
 
-router.post('/*', core);
+router.get("/", function (req, res) {
+  res.send("it works");
+});
+
+router.post("/*", core);
 
 router.listen(PORT, () =>
   console.log(`Example app listening on port ${PORT}!`)
